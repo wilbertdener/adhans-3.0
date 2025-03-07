0a4fb269f7e7b377126ae4c5c2dd5a06aceb03e8
wilbert <denerwilbert@gmail.com>
2025-03-07 20:39:38 -0300

    probabilidade funcionando

--- application/python/bdad.py
+++ application/python/bdad.py
@@ -9,6 +9,7 @@ import cv2
 
 from mahotas import gaussian_filter
 import os
+from collections import Counter
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -16,6 +17,24 @@ mydb = mysql.connector.connect(
   password="",
   database="bd_adhans"
 )
+
+def get_prob():
+  mycursor = mydb.cursor()
+  sql = """  
+  SELECT 
+      COUNT(*) AS total_exames,
+      SUM(CASE WHEN diagnostico = 1 THEN 1 ELSE 0 END) AS total_diagnostico,
+      SUM(CASE WHEN diagnostico_sistema = 1 AND diagnostico = 1 THEN 1 ELSE 0 END) AS total_diagnostico_sistema,  -- Aqui foi a mudança
+      SUM(CASE WHEN diagnostico = diagnostico_sistema THEN 1 ELSE 0 END) AS total_acertos
+  FROM exames;
+  """
+  mycursor.execute(sql)
+  myresult = mycursor.fetchone()  # Retorna (total_exames, total_diagnostico, total_diagnostico_sistema, total_acertos)
+
+  # Convertendo valores Decimal para inteiro
+  return tuple(int(x) for x in myresult)
+
+
 
 def get_fotos_by_exame(id_exame):
   mycursor = mydb.cursor()
@@ -58,55 +77,6 @@ def cria_objetos(id):
   
   return [foto1,foto2]
 
-
-#pegar as fotos, coordenadas e dimensões no bd - [id, coordenadas, local, tempo, dimensao]
-foto1,foto2=cria_objetos(1)#cria objeto com todos os dados que preciso
-print(foto1.id_foto)
-print(foto2.id_foto)
-print(foto1)
-    
-#imagem = cv2.imread(foto1.local)
-#altura, largura = imagem.shape[:2]
-#dimensao = f"{altura};{largura}"
-#print(cv2.__version__)
-
-caminho_absoluto = os.path.abspath(foto1.local)  # Converte para caminho absoluto
-print("caminho absoluto:")
-print(caminho_absoluto)
-
-caminho = "C:/wamp64/www"+foto1.local
-caminho2 = "C:/wamp64/www"+foto2.local
-
-img = cv2.imread(caminho)
-img2 = cv2.imread(caminho2)
-
-dimensao = str(img.shape[1])+";"+str(img.shape[0])
-dimensao2 = str(img2.shape[1])+";"+str(img2.shape[0])
-#if img is not None:
-#    cv2.waitKey(1)  # Pequeno delay antes de exibir a janela
-#    cv2.imshow('Imagem', img)
-#    cv2.waitKey(0)
-#    cv2.destroyAllWindows()
-#else:
-#    print("Erro: Imagem não carregada!")
-
-print("Dimensao registrada")
-print(foto1.dimensao)
-print("coordenadas registradas")
-print(foto1.coordenadas)
-print("Dimensao do arquivo 1")
-print(dimensao)
-
-
-
-
-#print(dimensao2)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-#print(caminho)
-
-
-
 #converter as coordenadas com as dimensões
 def corrigir_coordenadas(dimensao_antiga, dimensao_nova, coordenadas, lado):
     largura_antiga, altura_antiga = map(int, dimensao_antiga.split(";"))
@@ -128,7 +98,7 @@ def corrigir_coordenadas(dimensao_antiga, dimensao_nova, coordenadas, lado):
 
     return coordenadas_corrigidas, novo_lado
 
-
+#apenas para vizualizar, apagar depois
 def desenha(caminho,coordenadas, lado):
   
 
@@ -149,23 +119,192 @@ def desenha(caminho,coordenadas, lado):
       cv2.waitKey(0)
       cv2.destroyAllWindows()
 
-print("coordenadas calculadas")
-nova_coo,novo_lado = corrigir_coordenadas(foto1.dimensao,dimensao,foto1.coordenadas,10)
-print(nova_coo)
-print(novo_lado)
-desenha(caminho,nova_coo, novo_lado)
+
+
+
+def coleta_canais(img, cood,lado):
+  #hsv1 = cv2.cvtColor(roi1, cv2.COLOR_BGR2HSV)
+  #hsv2 = cv2.cvtColor(roi2, cv2.COLOR_BGR2HSV)
+  #hsv3 = cv2.cvtColor(roi3, cv2.COLOR_BGR2HSV)
+  imgfinal=[]
+  imgcut = img.copy()
+  for i in range(len(cood)):
+        
+    x, y = map(int, cood[i].split(";"))
+
+    x1 = int( x - (lado/2))
+    x2 = int( x + (lado/2))
+    y1 =int( y - (lado/2))
+    y2 = int( y + (lado/2))
+    
+    imgfinal.append(imgcut[y1:y2, x1:x2])
+    
+  
+  canalR1=[]
+  canalS1=[]
+  canalR2=[]
+  canalS2=[]
+  n=0
+  for roi in imgfinal:
+    
+    for x in range(roi.shape[0]):
+      for y in range(roi.shape[0]):
+        if(n<3):# Vai de 0 até x-1
+          
+          canalR1.append(roi[x][y][2])
+          
+          
+          hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+          canalS1.append(hsv[x][y][1])
+          
+        else:
+          
+          canalR2.append(roi[x][y][2])
+          
+          
+          hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+          canalS2.append(hsv[x][y][1])
+             
+        
+    n=n+1
+  
+  canalR1_comum = Counter(canalR1).most_common(1)[0][0]
+  canalR2_comum = Counter(canalR2).most_common(1)[0][0]
+
+  canalS1_comum = Counter(canalS1).most_common(1)[0][0]
+  canalS2_comum = Counter(canalS2).most_common(1)[0][0]
+  #canalRD2, canalHD2,canalRF2, canalHF2
+  return canalR1_comum,canalS1_comum,canalR2_comum,canalS2_comum
+      
+#print(coleta_canais(roidentro1[0]))     
+#depois.append(convertponto(pasta+"/" + nome, v))
+
+
+
+
 #pegar o valor de pixel mais comun naquelas coordenadas
 #tacar na tabela - pixel_comum
 
-#fazer a formula |dentro - fora|
-#fazer a formula |antes - depois|
 
-
-
-#filtros F1 e F2
-#F1 - em |dentro - fora| na foto 1 para a exclusão do grupo V -> S<40
-#F2 - em |antes - depois| para a seleção do grupo H -> R >= 10
 
 
 #probabilidade de acerto do sistema
 #salvar na tabela exames - probabilidade / diagnostico_sistema
+
+def main(id):
+  #pegar as fotos, coordenadas e dimensões no bd - [id, coordenadas, local, tempo, dimensao]
+  foto1,foto2=cria_objetos(id)#cria objeto com todos os dados que preciso
+
+  #iniciando algumas variaveis
+  #caminho_absoluto = os.path.abspath(foto1.local)  # Converte para caminho absoluto
+  #print("caminho absoluto:")
+  #print(caminho_absoluto)
+  
+  caminho = "C:/wamp64/www"+foto1.local
+  img1 = cv2.imread(caminho)
+  dimensao1 = str(img1.shape[1])+";"+str(img1.shape[0])
+  nova_coo1,novo_lado1 = corrigir_coordenadas(foto1.dimensao,dimensao1,foto1.coordenadas,10)
+  canalRD1, canalSD1,canalRF1, canalSF1 = coleta_canais(img1, nova_coo1,novo_lado1)
+  
+
+
+  caminho2 = "C:/wamp64/www"+foto2.local
+  img2 = cv2.imread(caminho2)
+  dimensao2 = str(img2.shape[1])+";"+str(img2.shape[0])
+  nova_coo2,novo_lado2 = corrigir_coordenadas(foto2.dimensao,dimensao2,foto2.coordenadas,10)
+  
+  canalRD2, canalSD2,canalRF2, canalSF2 = coleta_canais(img2, nova_coo2,novo_lado2)
+  
+
+  #print(str(imagemffff.shape[1])+";"+str(imagemffff.shape[0]))
+  #imgcut2 = img.copy()
+
+  #fazer a formula |dentro - fora|
+  F1_R = abs(int(canalRD1) - int(canalRF1))
+  F1_S = abs(int(canalSD1) - int(canalSF1))
+  
+  F2_R = abs(int(canalRD2) - int(canalRF2))
+  F2_S = abs(int(canalSD2) - int(canalSF2))
+  
+  T = abs(int(F1_R)-int(F2_R))
+  
+  prob = get_prob()
+  print(prob)
+  total_exames = prob[0]
+  diag_pos_med = prob[1]
+  diag_pos_sis_med = prob[2]
+  total_med_sis = prob[3]
+ 
+
+  print("Total de exames: "+str(total_exames))
+  print("Total de positivo med: "+str(diag_pos_med))
+  print("Total de positivo sis e med:  "+str(diag_pos_sis_med))
+  print("Total de med=sis:  "+str(total_med_sis))
+  
+  if(F1_S<40):
+    if(T>=10):
+      print("Vermelhidão com diferença relavante, pode ser hanseniase")
+      #sistema sim/medico sim
+      #(sistema= medico)/ total -acertividade do sistema
+      if(diag_pos_med!=0  ):
+        print("Probabilidade: "+ str(diag_pos_sis_med/diag_pos_med))
+        print("Acertividade: "+ str(total_med_sis/total_exames))
+      else:
+        print("Positivo med: "+ str(diag_pos_sis_med))
+        print("Positivo med: "+ str(diag_pos_med))
+        print("Acertividade: "+ str(total_med_sis/total_exames))  
+     
+      
+    else:
+      print("Vermelhidão sem diferença relavante, a lesão pode não ser hanseniase nem vitigo") 
+      #sistema não/medico não
+      #(sistema= medico)/ total -acertividade do sistema
+      if(diag_pos_med!=0  ):
+        print("Probabilidade: "+ str(1-(diag_pos_sis_med/diag_pos_med)))
+        print("Acertividade: "+ str(total_med_sis/total_exames))
+      else:
+        print("Positivo med: "+ str(diag_pos_sis_med))
+        print("Positivo med: "+ str(diag_pos_med))
+        print("Acertividade: "+ str(total_med_sis/total_exames))  
+  else:
+    print("Lesão muito clara, pode não ser hanseniase, talvez vitiligo")  
+    if(diag_pos_med!=0):
+      print("Probabilidade: "+ str(1-(diag_pos_sis_med/diag_pos_med)))
+      print("Acertividade: "+ str(total_med_sis/total_exames))
+    else:
+      print("Positivo med: "+ str(diag_pos_sis_med))
+      print("Positivo med: "+ str(diag_pos_med))
+      print("Acertividade: "+ str(total_med_sis/total_exames))    
+  
+  
+  
+  
+  
+  
+  #fazer a formula |antes - depois|
+
+
+
+  #filtros F1 e F2
+  #F1 - em |dentro - fora| na foto 1 para a exclusão do grupo V -> S<40
+  #F2 - em |antes - depois| para a seleção do grupo H -> R >= 10
+
+  #cv2.imshow("Roi H", cv2.cvtColor(roidentro1, cv2.COLOR_BGR2HSV))
+  #desenha(caminho,nova_coo, novo_lado)
+  #cv2.waitKey(0)
+  #cv2.destroyAllWindows()
+
+  
+  
+main(3)
+
+
+
+
+
+
+
+
+
+
+
